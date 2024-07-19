f831ab7e1ab1f3e71845842a09e990b8a66878a3
ned <ned@ubuntu>
2024-07-19 18:39:02 -0400

    add files

---
+++ README.md
@@ -0,0 +1,8 @@
+stuff to install:
+
+neovim
+i3
+rofi or dmenu
+nitrogen
+xfce4 xfce4-goodies
+i3ipc-glib
---
+++ update_dots.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+cp -r ~/.config/nvim .
+cp -r ~/.config/i3 .
+cp -r ~/.config/polybar .
+cp -r ~/.config/alacritty .
